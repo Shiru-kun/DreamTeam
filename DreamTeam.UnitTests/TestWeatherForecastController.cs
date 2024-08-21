b238a72bf045493bd667865b2eb9351255e53f14
Focas Kandulo <focas.kandulo@ambisig.com>
2024-08-21 22:20:27 +0200

    Add project files.

---
+++ DreamTeam.UnitTests/TestWeatherForecastController.cs
@@ -0,0 +1,44 @@
+﻿
+using DreamTimeAPI;
+using DreamTimeAPI.Controllers;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace DreamTeam.UnitTests
+{
+    public class TestWeatherForecastController
+    {
+        private Mock<ILogger<WeatherForecastController>> _logger = new();
+
+        [Test]
+        public void Should_Return_Weather_Forecast_For_Five_Days()
+        {
+            //Arrange
+            var weatherForeCastController = new WeatherForecastController(_logger.Object);
+
+            //Act 
+            var result = weatherForeCastController.Get();
+
+           //Assert
+          result.Count()
+                .Should()
+                .Be(5);
+          Assert.AreEqual(5, result.Count());
+        }
+        [Test]
+        public void Should_Return_List_Of_WeatherForecast()
+        {
+            //Arrange
+            var weatherForeCastController = new WeatherForecastController(_logger.Object);
+
+            //Act 
+            var result = weatherForeCastController.Get().ToList();
+
+            //Assert
+            result
+                  .Should()
+                  .BeOfType<List<WeatherForecast>>();
+        }
+    }
+}
---
+++ DreamTeam.UnitTests/UnitTest1.cs
@@ -0,0 +1,21 @@
+namespace DreamTeam.UnitTests
+{
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+            // arrange / preparar 
+        {     int a = 1; int b = 2;
+            // act     / agir 
+            int resultado = Somar(a, b);
+            // assert  verificar/ avaliar
+            Assert.AreEqual(3,resultado);
+        }
+
+
+        private int Somar(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
